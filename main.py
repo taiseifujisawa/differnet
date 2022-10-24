8f9e9abb9e61d68b7c6241678ff5e5e7500a1b95
taiseifujisawa <fujisawa-taisei-bc@ynu.jp>
2022-10-24 14:43:38 +0900

    add start time, end time, and delta

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ import config as c
 from train import train
 from utils import load_datasets, make_dataloaders
 import logging_controller
+from datetime import datetime
 
+start_time = datetime.now()
+print(f"start at {start_time.strftime('%Y-%m-%d_%H-%M-%S')}")
 train_set, test_set = load_datasets(c.dataset_path, c.class_name)
 train_loader, test_loader = make_dataloaders(train_set, test_set)
 model = train(train_loader, test_loader)
+end_time = datetime.now()
+print(f"end at {end_time.strftime('%Y-%m-%d_%H-%M-%S')}")
+print(f"It took {end_time - start_time} to complete.")
